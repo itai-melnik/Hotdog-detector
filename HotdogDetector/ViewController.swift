6518ad44d5326993af924eee00666f21a5d803f4
Itai-Melnik-RUNI <154412942+Itai-Melnik-RUNI@users.noreply.github.com>
2024-10-10 11:04:51 +0300

    Initial Commit

---
+++ HotdogDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotdogDetector
+//
+//  Created by Itai Melnik on 10/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HotdogDetectorTests/HotdogDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotdogDetectorTests.swift
+//  HotdogDetectorTests
+//
+//  Created by Itai Melnik on 10/10/2024.
+//
+
+import Testing
+@testable import HotdogDetector
+
+struct HotdogDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
